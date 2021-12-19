4b922fb7290318eddf4b8927469083fe7df5e29c
Facundo Rodriguez Sabia <facundorsabia@gmail.com>
2021-12-19 17:57:19 -0300

    Actualizacion Contact Form

--- js/main.js
+++ js/main.js
@@ -36,3 +36,25 @@ jQuery(function($) {
 new WOW().init();
 
 
+//Contact Form
+
+
+const $form = document.querySelector("#form");
+
+$form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event){
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action,{
+    method: this.method, 
+    body: form, 
+    headers: {
+      "Accept": "application/json"
+    }
+  })
+  if (response.ok){
+    this.reset()
+    console.log("Gracias por contactarme, te escribire pronto");
+  }
+}
